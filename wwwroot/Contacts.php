1121b8a35f37e8945fc2c2992ca2d471a6ff0a35
psyam12 <psyam12@exmail.nottingham.ac.uk>
2018-06-12 15:12:49 +0100

    Added contact page back

---
+++ wwwroot/Contacts.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+		<link rel="stylesheet" href="css/contact.css">
+		<link rel="stylesheet" href="css/accordion.css">
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/accordion.js"></script>
+
+		<title>Contact | University of Nottingham Gliding club</title>
+		<link rel="icon" href="images/icon.png" type="image/png" sizes="16x16"> 
+		<meta name="keywords" content="university, nottingham, gliding, aviation, flying, club, sports, contacts, contact, contacting">
+		<meta name="description" content="Contacts for University of Nottingham Gliding club">
+	</head>
+	
+	<body>
+
+<!----------------------------------------------------Navbar-------------------------------------------------------------->
+		<?php include 'navbar.html'; ?>
+<!---------------------------------------------------/Navbar-------------------------------------------------------------->
+	
+		<div class="container-fluid paragraph content textbox">
+			<h3>Contact</h3>
+		<div class="container-fluid">
+			<p>University of Nottingham Gliding Club can be contacted in any of the following ways:</p>
+			<h4>Postal</h4>
+			<p>University of Nottingham Gliding Club</p>
+			<p>c/o Students' Union</p>
+			<p>Portland Building</p>
+			<p>University Park</p>
+			<p>Nottingham</p>
+			<p>NG7 2RD</p>
+			<h4>Social Media</h4>
+			<h4> 
+				<a href="https://www.facebook.com/groups/NUGlidingClub" >Facebook Group</a>
+			</h4>
+			<h4>Email</h4>
+			<p>For general information, enquries and business: info@nugc.net</p>
+			<p>Webmaster: psyam12@nottingham.ac.uk</p>
+		</div>
+		</div>
+
+
+
+</body>
+</html>
