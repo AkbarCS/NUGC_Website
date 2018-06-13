6d6a5bfd3effd8df4fc1e96f0bebeefa40a1a291
psyam12 <psyam12@exmail.nottingham.ac.uk>
2018-06-13 20:58:28 +0100

    Restructured website information and updated signup info page

--- wwwroot/FAQ.php
+++ wwwroot/FAQ.php
@@ -38,7 +38,7 @@
        <a class="accordion-section-title" href="#accordion-1">Do I need any experience to fly?</a> 
         <div id="accordion-1" class="accordion-section-content">
 			<p>No, you do not need any experience at all to fly.</p>
-			<p>Many of our members had never even seen a glider before they came flying with us, let alone flown one. We have access to an extensive fleet of gliders which cater for training (we have access to 4 2 seaters), early solo flying and competition racing (5 single seaters in total).</p>
+			<p>Many of our members had never even seen a glider before they came flying with us, let alone flown one. We have access to an extensive fleet of gliders which cater for training (we have access to four twin seater aircraft), early solo flying and competition racing (5 single seaters in total).</p>
 			<p>You will be instructed by both our own, and Cranwell Gliding Club's instructors, who can train anyone to fly solo. It's easier than driving a car!</p>
         </div><!--end .accordion-section-content-->
     </div><!--end .accordion-section-->
@@ -47,25 +47,15 @@
        <a class="accordion-section-title" href="#accordion-2">Is gliding safe?</a> 
         <div id="accordion-2" class="accordion-section-content">
 			<p>Yes, gliding is perfectly safe.</p>
-			<p>There are a lot of misconceptions about gliding - the fact that is has no engine worries some people, while others are under the impression that the glider needs wind to fly, and are worried that the wind might stop suddenly. Neither of these points are anything to worry about - lack of an engine doesn't mean the glider can't fly (see the 'How Gliders Fly' page for a detailed explanation), and a glider will fly just as well on a calm day as it will on a day with a breeze.</p>
+			<p>There are a lot of misconceptions about gliding - the fact that is has no engine worries some people, while others are under the impression that the glider needs wind to fly, and are worried that the wind might stop suddenly. Neither of these points are anything to worry about - lack of an engine doesn't mean the glider can't fly (see the <a href="How gliders fly.php" target="_blank">following</a> page for a detailed explanation), and a glider will fly just as well on a calm day as it will on a day with a breeze.</p>
 			<p>In reality, gliding is no more dangerous than driving. Every precaution is taken at Cranwell Gliding Club to ensure the safety of both you and other people. The club is full of very experienced pilots and instructors, and the equipment is maintained to extremely strict standards, and checked every single day before flying.</p>
 			<p>In short, there is nothing to worry about.</p>
         </div><!--end .accordion-section-content-->
     </div><!--end .accordion-section-->
     
   	<div class="accordion-section">
-       <a class="accordion-section-title" href="#accordion-3">How do I join?</a> 
+       <a class="accordion-section-title" href="#accordion-3">Who is allowed to join?</a> 
         <div id="accordion-3" class="accordion-section-content">
-			<p>You can join UoN Gliding at Freshers' Fayre, on our Tuesday night socials, at Refresher's Fayre (in January, after exams)</p>
-			<p>We will be at Freshers' Fayre for all 3 days, and have both a stall inside the tent, and a glider out on the sports field. You can ask any club member there about joining and they will redirect you to the right place.</p>
-			<p>Alternatively, if you want to join after freshers' fayre, you can come along to any of our Tuesday night socials - every Tuesday during term time at 7:30 in Mooch.</p>
-			<p>If you can't make any of these events, please use the Contact link at the top of the page or to the left, and get in touch. We will be able to advise you on how to join the club.</p>
-        </div><!--end .accordion-section-content-->
-    </div><!--end .accordion-section-->
-    
-  	<div class="accordion-section">
-       <a class="accordion-section-title" href="#accordion-4">Who is allowed to join?</a> 
-        <div id="accordion-4" class="accordion-section-content">
 			<p>Any Full or Associate member of the Students' Union can join UoN Gliding.</p>
 			<p>All students of the university of Nottingham are automatically full members of the SU, and can join the club at any point in the year.</p>
 			<p>After graduation, alumni may pay the Students' Union for associate membership, either £5 per year or £30 for life membership currently.</p>
@@ -74,8 +64,8 @@
     </div><!--end .accordion-section-->
     
   	<div class="accordion-section">
-       <a class="accordion-section-title" href="#accordion-5">When do you fly?</a> 
-        <div id="accordion-5" class="accordion-section-content">
+       <a class="accordion-section-title" href="#accordion-4">When do you fly?</a> 
+        <div id="accordion-4" class="accordion-section-content">
 			<p>We fly every weekend during term time, and most weekends during the holidays as well, depending on transport.</p>
 			<p>During term time, NUGC organise transport for members to get to the airfield every weekend.</p>
 			<p>During the holidays, members organise their own transport, usually via our forum. There is usually someone going on a given weekend, so it is worth checking in the forum if you want to fly during the holidays.</p>
@@ -83,8 +73,8 @@
     </div><!--end .accordion-section-->
     
   	<div class="accordion-section">
-       <a class="accordion-section-title" href="#accordion-6">How do you get there?</a> 
-        <div id="accordion-6" class="accordion-section-content">
+       <a class="accordion-section-title" href="#accordion-5">How do you get there?</a> 
+        <div id="accordion-5" class="accordion-section-content">
 			<p>NUGC organises transport to RAF Cranwell every weekend during term time.</p>
 			<p>Any member with a car is welcome to drive people to the airfield for us, and will get their petrol costs paid for by the club.</p>
 			<p>We take £5 off each person going flying to cover transport costs, and then pay drivers for each return trip to the airfield.</p>
@@ -94,18 +84,18 @@
 	</div><!--end .accordion-section-->
 	
   	<div class="accordion-section">
-       <a class="accordion-section-title" href="#accordion-7">How much does it cost?</a> 
-        <div id="accordion-7" class="accordion-section-content">
-			<p>Gliding is actually surprisingly cheap! Membership of the club is only £49 for the entire year, and flights are only £5 per launch!</p>
+       <a class="accordion-section-title" href="#accordion-6">How much does it cost?</a> 
+        <div id="accordion-6" class="accordion-section-content">
+			<p>Gliding is actually surprisingly cheap! Membership of the club is only £49 for the entire year, and flights are only £5 per launch! For those joining between February and August, membership is only £30. Membership lasts until the end of August.</p>
 			<p>Because we are affiliated to an RAFGSA club, the fees are amongst the cheapest in the country for gliding.</p>
-			<p>More information can be found on our prices page.</p>
+			<p>More information can be found on our <a href="Prices.php" target="_blank">prices</a> page.</p>
     	</div><!--end .accordion-section-content-->
 	</div><!--end .accordion-section-->
 	
   	<div class="accordion-section">
-       <a class="accordion-section-title" href="#accordion-8">How long does it take to learn?</a> 
-        <div id="accordion-8" class="accordion-section-content">
-			<p>The average number of flights in a 2 seater before someone is sent solo is around 60. This can vary quite a lot however - we have some people who only needed 40, and some who need more.</p>
+       <a class="accordion-section-title" href="#accordion-7">How long does it take to learn?</a> 
+        <div id="accordion-7" class="accordion-section-content">
+			<p>The average number of flights in a twin seater before someone is sent solo is around 60. This can vary quite a lot however - we have some people who only needed 40, and some who need more.</p>
 			<p>The legal minimum number of flights a person must have is 20, but it is unlikely that someone will reach solo standard within this time, unless they have had previous experience.</p>
 			<p>Because you will fly in batches of 3 flights, it is entirely possible to join us at the beginning of the year, and be solo in time for the good summer weather.</p>
     	</div><!--end .accordion-section-content-->
